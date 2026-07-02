7f256ea127860dee473047146fc76e7e3003b758
Jashuva-Billa <jashu1150@gmail.com>
2026-07-02 09:32:21 -0700

    Remove run.bat launchers

--- copilot_app/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\run.ps1"
-pause
--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0copilot_app"
-powershell -NoProfile -ExecutionPolicy Bypass -File "scripts\run.ps1"
-pause
